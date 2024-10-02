c9c4f5d0e0954a64db07a06e6e21d5a5d5dc993c
venkat <venkatravilla999@gmail.com>
2024-10-02 20:26:19 +0530

    basics completed

---
+++ index.js
@@ -0,0 +1,35 @@
+//  primitives
+
+var a = 'sachin'
+
+console.log(a, typeof a)
+
+var b = 40
+
+console.log(b, typeof b)
+
+var c = true
+console.log(c, typeof c)
+var d 
+console.log(d, typeof d)
+
+var e = null
+console.log(e, typeof e)
+
+//reference
+
+var f = {
+  name: 'sachin',
+  age: 40
+}
+console.log(f, typeof f)
+
+var g = [1, 2, 3, 4, 5]
+console.log(g, typeof g)
+console.log(Array.isArray(g))
+
+
+var h = () => {
+  
+}
+console.log(h, typeof h)
