ccde78e743737864daec994195c7203abba16ea8
venkat <venkatravilla999@gmail.com>
2024-10-04 20:29:18 +0530

    operators

---
+++ Operators/index.js
@@ -0,0 +1,84 @@
+
+
+//Arithmatic
+
+var a = 20
+
+var b = 10
+
+// console.log( a + b)
+// console.log( a - b)
+// console.log( a * b)
+// console.log( a / b)
+// console.log( a % b)
+// console.log( a ** b)
+
+// console.log(++a) //pre increment
+// console.log(++a) //pre increment
+// console.log(++a) //pre increment
+// console.log(a++) //post increment
+// console.log(a)
+// console.log(--a) //pre decrement
+// console.log(--a) //pre decrement
+// console.log(a--) //pre decrement
+// console.log(a)
+
+// console.log(++a) //21
+// console.log(a++) //21
+// console.log(--a)//21
+// console.log(a--)//21
+// console.log(a)
+
+//assignment operator
+
+// var a = 20
+// var b = 100
+// console.log(a)
+// console.log(b)
+// a = b
+// console.log(a)
+// console.log(b)
+
+// var a = 20
+
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+
+// console.log(a += 5)
+// console.log(a -= 5)
+// console.log(a *= 5)
+// console.log(a /= 5)
+// console.log(a %= 5)
+// console.log(a **= 5)
+
+//comparison operators
+
+// var a = 10
+// var b = 11
+
+//Relational
+// console.log(a > b)
+// console.log(a >= b)
+// console.log(a < b)
+// console.log(a <= b)
+
+// Equality
+
+// var a = 10
+// var b = 11
+// var b = 10
+// var b = '10'
+// var a = undefined
+// var b = null
+// var a = {}
+// var b = {}
+// var b = {value: 10}
+// var b = a
+var a = [1, 2, 3]
+var b = [1, 2, 3]
+
+console.log(a == b) //loose
+console.log(a === b) //strict
